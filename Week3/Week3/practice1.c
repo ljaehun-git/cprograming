a98d0d4b31dde5d491d0c76c82bf14b16de870dd
ljaehun-git <ljaehun0719@gmail.com>
2026-09-18 18:23:41 +0900

    Week3 Practice 1

---
+++ Week3/Week3/practice1.c
@@ -0,0 +1,12 @@
+﻿#include <stdio.h>
+#include <windows.h>
+
+int main(void)
+{
+    printf("Hello world!!\n");
+    printf("서울시립대학교\n");
+    printf("자유전공학부\n");
+    printf("\"2026940116\" 임재훈\n");
+
+    return 0;
+}
---
+++ Week3/practice1.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main(void)
+{
+    printf("Hello world!!\n");
+
+    printf("서울시립대학교\n");
+    printf("전자전기컴퓨터공학부\n");
+    printf("\"2026940116임재훈\" 임재훈\n");
+
+    return 0;
+}
